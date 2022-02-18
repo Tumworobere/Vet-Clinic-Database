42f4cb990aa8d172e696110639b766ae6883fc64
Tumworobere <annaht.annah@gmail.com>
2022-02-18 11:45:28 +0300

    Update Schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
+CREATE TABLE animals (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL NOT NULL 
+);
