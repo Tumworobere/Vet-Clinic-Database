03af0c811557e9fa891d5a8a97b2e7815a497c5f
Tumworobere <annaht.annah@gmail.com>
2022-02-19 00:56:19 +0300

    Update:Schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,47 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species varchar(100);
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- Create a table named owners with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- full_name: string
+-- age: integer  
+
+CREATE TABLE owners (
+id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+full_name varchar(100),
+age INT,
+PRIMARY KEY (id)
+);
+
+-- Create a table named species with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+
+CREATE TABLE species (
+id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+name varchar(100),
+PRIMARY KEY (id)
+);
+
+-- Modify animals table:
+-- Make sure that id is set as autoincremented PRIMARY KEY
+-- Remove column species
+-- Add column species_id which is a foreign key referencing species table
+-- Add column owner_id which is a foreign key referencing the owners table
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
