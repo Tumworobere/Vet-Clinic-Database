749a7629ddb8d639a3845d383aac945ae0f11b51
Tumworobere <annaht.annah@gmail.com>
2022-02-19 09:15:49 +0300

    Implement required changes

--- data.sql
+++ data.sql
@@ -1,9 +1,4 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
-
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_Kg) VALUES ('Agumon', '2020-02-03' , 0 , true , 10.23);
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_Kg) VALUES ('Gabumon', '2018-11-15' , 2 , true , 8.0);
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_Kg) VALUES ('Pikachu', '2021-01-07' , 1 , false , 15.04);
@@ -15,6 +10,7 @@ INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_Kg) VALUE
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_Kg) VALUES ('Angemon', '2005-06-12' , 1 , true , -45);
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_Kg) VALUES ('Boarmon', '2005-06-07' , 7 , true , 20.4);
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_Kg) VALUES ('Blossom', '1998-10-13' , 3 , true , 17);
+
 ----------------------------------------------------------------------------------------------------------------------------------
 INSERT INTO owners(full_name,age) VALUES ('Sam Smith',34),('Jennifer Orwell',19),
 ('Bob',45),('Melody Pond',77),('Dean Winchester',14),('Jodie Whittaker',38);
